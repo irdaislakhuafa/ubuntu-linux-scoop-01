b26616d424939107fa775146761956fa647143c4
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-10-22 16:45:02 +0700

    add assets

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ubuntu Linux Scoop
+- integrate Gnome Shell Extensions with browser (Firefox/Chrome)
+    - url : [https://extensions.gnome.org/](https://extensions.gnome.org/)  
+    - click `Click here to install browser extension`
--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/bash
 source ./colors.sh
 
+workDir="$(pwd)"
+
 printlnGreen "=> updating packages"
 apt update && apt dist-upgrade -y
+
+printlnGreen "=> installing gnome-tweaks"
+apt install gnome-tweaks -y
+
+printlnGreen "=> install WhiteSur GTK Theme"
+apt install git
+cd "$workDir/assets/themes" 
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
