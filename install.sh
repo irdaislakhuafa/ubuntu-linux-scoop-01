f7e57170234e1fe0801881bdafe33e7cd8b8a18a
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-10-22 17:38:16 +0700

    install fonts

--- install.sh
+++ install.sh
@@ -12,29 +12,35 @@ separator="=>"
 # printlnGreen "$separator installing gnome-tweaks"
 # $access apt install gnome-tweaks -y
 
-printlnGreen "$separator install WhiteSur GTK Theme"
-$access apt install git -y;
-if ! [ -d "$workDir/assets/themes" ]; then
-    mkdir -v "$workDir/assets/themes"
-fi
-cd "$workDir/assets/themes"
-git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
-cd "WhiteSur-gtk-theme" 
-chmod +x * -R
-./install.sh -t red
-cd ..
-rm -rf "WhiteSur-gtk-theme"
-cd "$workDir"
+# printlnGreen "$separator install WhiteSur GTK Theme"
+# $access apt install git -y;
+# if ! [ -d "$workDir/assets/themes" ]; then
+#     mkdir -v "$workDir/assets/themes"
+# fi
+# cd "$workDir/assets/themes"
+# git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
+# cd "WhiteSur-gtk-theme" 
+# chmod +x * -R
+# ./install.sh -t red
+# cd ..
+# rm -rf "WhiteSur-gtk-theme"
+# cd "$workDir"
 
-printlnGreen "$separator download and install reversal icon"
-if ! [ -d "$workDir/assets/icons" ]; then
-    mkdir -v "$workDir/assets/icons"
+# printlnGreen "$separator download and install reversal icon"
+# if ! [ -d "$workDir/assets/icons" ]; then
+#     mkdir -v "$workDir/assets/icons"
+# fi
+# cd "$workDir/assets/icons"
+# git clone https://github.com/yeyushengfan258/Reversal-icon-theme.git
+# cd "Reversal-icon-theme"
+# chmod +x * -R
+# ./install.sh -red
+# cd ..
+# rm -rf "Reversal-icon-theme"
+# cd "$workDir"
+
+printlnGreen "$separator installing fonts"
+if ! [ -d ~/.local/share/ ]; then
+    mkdir -pv ~/.local/share/
 fi
-cd "$workDir/assets/icons"
-git clone https://github.com/yeyushengfan258/Reversal-icon-theme.git
-cd "Reversal-icon-theme"
-chmod +x * -R
-./install.sh -red
-cd ..
-rm -rf "Reversal-icon-theme"
-cd "$workDir"
+cp -rv "$workDir/assets/fonts/" ~/.local/share/
